95d612906857acee3078997c322ea6c35425341e
Bbbbbbianca <1104191069@qq.com>
2021-04-24 18:42:21 +0800

    update pages

--- src/app.config.js
+++ src/app.config.js
@@ -1,6 +1,14 @@
 export default {
   pages: [
-    'pages/index/index'
+    'pages/index/index',
+    'pages/mine/mine',
+    'pages/course/course',
+    'pages/plan/plan',
+    'pages/car/car',
+    'pages/dailytask/dailytask',
+    'pages/detail/detail',
+    'pages/material/material',
+    'pages/schedule/schedule'
   ],
   window: {
     backgroundTextStyle: 'light',
---
+++ src/base/constant.ts
@@ -0,0 +1,11 @@
+export const APP_ROUTES = {
+    INDEX: 'pages/index/index',
+    CAR: 'pages/car/car',
+    COURSE: 'pages/course/course',
+    DAILYTASK: 'pages/dailytask/dailytask',
+    DETAIL: 'pages/detail/detail',
+    MATERIAL: 'pages/material/material',
+    MINE: 'pages/mine/mine',
+    PLAN: 'pages/plan/plan',
+    SCHEDULE: 'pages/schedule/schedule'
+}
