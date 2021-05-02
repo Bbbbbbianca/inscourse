ca4e4033294f7a007fa897192954d93752eaef06
Bbbbbbianca <1104191069@qq.com>
2021-05-02 13:45:33 +0800

    2. update course( not finished

--- src/pages/course/course.jsx
+++ src/pages/course/course.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { View, Image } from '@tarojs/components'
 import { AtSearchBar } from 'taro-ui'
 import './course.scss'
+import heatPic from '../../assets/images/heat.png'
 
 // function getCourses () {
 //   // TO DO
@@ -12,10 +13,11 @@ export default class Course extends Component {
     super(props);
     this.state = {
       searchCourse: '',
+      // courses: getCourses(),
       courses: [
-        {courseId: 1, courseImg: '', courseName: '数学分析', courseDct: '好学', courseHeat: 234},
-        {courseId: 2, courseImg: '', courseName: '线性代数', courseDct: '好学', courseHeat: 834},
-        {courseId: 3, courseImg: '', courseName: '大学物理', courseDct: '好学', courseHeat: 254}
+        {courseId: 1, courseImg: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1605983591981&di=b075a1308a8228ac2e016f0b04c44e63&imgtype=0&src=http%3A%2F%2Fp6.itc.cn%2Fmpbp%2Fpro%2F20200927%2Ffc5dd7d801304fdb83b9f37c07ae97ae.jpeg', courseName: '数学分析', courseDct: '好学', courseHeat: 234},
+        {courseId: 2, courseImg: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1605983591981&di=b075a1308a8228ac2e016f0b04c44e63&imgtype=0&src=http%3A%2F%2Fp6.itc.cn%2Fmpbp%2Fpro%2F20200927%2Ffc5dd7d801304fdb83b9f37c07ae97ae.jpeg', courseName: '线性代数', courseDct: '学得很快乐', courseHeat: 834},
+        {courseId: 3, courseImg: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1605983591981&di=b075a1308a8228ac2e016f0b04c44e63&imgtype=0&src=http%3A%2F%2Fp6.itc.cn%2Fmpbp%2Fpro%2F20200927%2Ffc5dd7d801304fdb83b9f37c07ae97ae.jpeg', courseName: '大学物理', courseDct: '这你不能不会吧', courseHeat: 254}
       ],
     }
   }
@@ -43,20 +45,22 @@ export default class Course extends Component {
             src={course.courseImg}
             className='course-item-img'
           />
-          <View
-            className='course-item-title'
-          >
-            {course.courseName}
-          </View>
-          <View
-            className='course-item-dct'
-          >
-            {course.courseDct}
-          </View>
-          <View
-            className='course-item-heat'
-          >
-            {course.courseHeat}
+          <View className='course-item-text'>
+            <View className='course-item-title'>
+              {course.courseName}
+            </View>
+            <View className='course-item-dct'>
+              {course.courseDct}
+            </View>
+            <View className='course-item-heat-part'>
+              <Image
+                src={heatPic}
+                className='course-item-heat-img'
+              />
+              <View className='course-item-heat'>
+                {course.courseHeat}
+              </View>
+            </View>
           </View>
         </View>  
       )
