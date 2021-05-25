c46368b031b2c3fd6b629595b147a9e47ca4f56d
panxingyu.icer <panxingyu.icer@bytedance.com>
2021-05-25 15:17:35 +0800

    feat: add group and schedule pages

--- src/pages/group/group.jsx
+++ src/pages/group/group.jsx
@@ -11,25 +11,33 @@ export default class Group extends Component {
       mates: [
         {
           mate_id: 1,
-          course_img: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1605983591981&di=b075a1308a8228ac2e016f0b04c44e63&imgtype=0&src=http%3A%2F%2Fp6.itc.cn%2Fmpbp%2Fpro%2F20200927%2Ffc5dd7d801304fdb83b9f37c07ae97ae.jpeg',
+          course: '数学分析',
           mate_img: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1605983591981&di=b075a1308a8228ac2e016f0b04c44e63&imgtype=0&src=http%3A%2F%2Fp6.itc.cn%2Fmpbp%2Fpro%2F20200927%2Ffc5dd7d801304fdb83b9f37c07ae97ae.jpeg',
-          mystatus: 0,
-          opstatus: 1,
-
+          mate: 'abababab',
+          finished: 1,
+          notfinished: 2,
+          content: '读第一章',
+          date: '2021-5-31'
         },
         {
           mate_id: 2,
-          course_img: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1605983591981&di=b075a1308a8228ac2e016f0b04c44e63&imgtype=0&src=http%3A%2F%2Fp6.itc.cn%2Fmpbp%2Fpro%2F20200927%2Ffc5dd7d801304fdb83b9f37c07ae97ae.jpeg',
+          course: '数学分析',
           mate_img: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1605983591981&di=b075a1308a8228ac2e016f0b04c44e63&imgtype=0&src=http%3A%2F%2Fp6.itc.cn%2Fmpbp%2Fpro%2F20200927%2Ffc5dd7d801304fdb83b9f37c07ae97ae.jpeg',
-          mystatus: 1,
-          opstatus: 0,
+          mate: '路人甲',
+          finished: 1,
+          notfinished: 2,
+          content: '读第一章',
+          date: '2021-5-31'
         },
         {
           mate_id: 3,
-          course_img: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1605983591981&di=b075a1308a8228ac2e016f0b04c44e63&imgtype=0&src=http%3A%2F%2Fp6.itc.cn%2Fmpbp%2Fpro%2F20200927%2Ffc5dd7d801304fdb83b9f37c07ae97ae.jpeg',
+          course: '数学分析',
           mate_img: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1605983591981&di=b075a1308a8228ac2e016f0b04c44e63&imgtype=0&src=http%3A%2F%2Fp6.itc.cn%2Fmpbp%2Fpro%2F20200927%2Ffc5dd7d801304fdb83b9f37c07ae97ae.jpeg',
-          mystatus: 2,
-          opstatus: 1,
+          mate: '路人甲',
+          finished: 1,
+          notfinished: 2,
+          content: '读第一章',
+          date: '2021-5-31'
         }
       ],
     };
@@ -52,42 +60,26 @@ export default class Group extends Component {
               className='group-item'
               onClick={()=>{this.toShowMatedetail(mate.mate_id)}}
             >
-              <Image
-                src={mate.course_img}
-                className='group-course-img'
-              />
-              <View className='group-text'>
-                <View>
-                  {
-                    mate.mystatus == 0
-                    ?
-                    <View className='group-mystatus-complete'>
-                      今日待办已完成～
-                    </View>
-                    :
-                    <View className='group-mystatus-tip'>
-                      { '今日仍有' + mate.mystatus + '项任务待打卡！'}
-                    </View>
-                  }
-                </View>
-                <View>
-                  {
-                    mate.opstatus == 0
-                    ?
-                    <View className='group-opstatus-complete'>
-                      对方今日待办已完成～
-                    </View>
-                    :
-                    <View className='group-opstatus-tip'>
-                      { '对方今日仍有' + mate.opstatus + '项任务待打卡！'}
-                    </View>
-                  }
+              <View className='group-mate'>
+                <Image
+                  src={mate.mate_img}
+                  className='group-mate-img'
+                />
+                <View className='group-mate-name'>
+                  {mate.mate}
                 </View>
               </View>
-              <Image
-                src={mate.mate_img}
-                className='group-mate-img'
-              />
+              <View className='group-text'>
+                <View className='group-text-course'>
+                  {mate.course}
+                </View>
+                <View className='group-text-progress'>
+                  {'已打卡任务' + mate.finished + '项，未打卡任务' + mate.notfinished + '项'}
+                </View>
+                <View className='group-text-tip'>
+                  {'记得在' + mate.date + '前完成打卡任务：' + mate.content}
+                </View>
+              </View>
             </View>
           ))}
         </View>
